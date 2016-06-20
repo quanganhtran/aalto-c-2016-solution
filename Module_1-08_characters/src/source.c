e77162a7ef5845b60fff589a1794eb64c458b4db
Anh <quanganh.tran94@gmail.com>
2016-06-21 00:24:16 +0300

    1-08

--- Module_1-08_characters/src/source.c
+++ Module_1-08_characters/src/source.c
@@ -2,28 +2,41 @@
 #include <ctype.h>
 #include <string.h>
 #include "source.h"
-
+ 
 /* Don't touch the definition of msgs array! Checker uses this. */
 char *msgs[10] = {
     "'6=*w+~)._", "J65+~5+~=0/*69,~+9;,9*~19++=79"
 };
-
-
+ 
+ 
 void ascii_chart(char min, char max)
 {
+    for (int i = min; i <= max; i++) {
+        printf("%3d 0x%02x ", i, i);
+        if (isprint(i)) printf("%c", i);
+        else printf("?");
+        if ((i - min + 1) % 4) printf("\t");
+        else printf("\n");
+    }
 }
-
+ 
 char get_character(int msg, unsigned int cc) {
     if (msg >= 10 || !msgs[msg])
         return 0;
-
+ 
     if (strlen(msgs[msg]) <= cc)
         return 0;
-    
+     
     return msgs[msg][cc];
 }
-
-
+ 
+ 
 void secret_msg(int msg)
 {
+    int i = 0;
+    char c = get_character(msg, i);
+    while(c) {
+        printf("%c", 158 - c);
+        c = get_character(msg, ++i);
+    }
 }

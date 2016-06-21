0c819bc9c6e312dd6cccf6f81e1b0a02e1f2f34e
quanganhtran <quanganhtran@users.noreply.github.com>
2016-06-21 10:55:08 +0300

    1-02,04,05

--- Module_1-04_func/src/source.c
+++ Module_1-04_func/src/source.c
@@ -1,2 +1,5 @@
 #include <math.h>
-
+ 
+double vectorlength(double x, double y, double z) {
+    return sqrt(x*x + y*y + z*z);
+}
--- Module_1-05_calc/src/source.c
+++ Module_1-05_calc/src/source.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 #include "source.h"
-
-
-
+ 
+ 
+ 
 void simple_sum(void)
 {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d\n", a, b, a + b);
 }
-
-
+ 
+ 
 void simple_math(void)
 {
+    float a, b;
+    char o;
+    scanf("%f %c %f", &a, &o, &b);
+    switch(o) {
+        case '+':
+            printf("%.1f", a + b);
+            break;
+        case '-':
+            printf("%.1f", a - b);
+            break;
+        case '*':
+            printf("%.1f", a * b);
+            break;
+        case '/':
+            printf("%.1f", a / b);
+            break;
+        default:
+            printf("ERR");
+    }
 }

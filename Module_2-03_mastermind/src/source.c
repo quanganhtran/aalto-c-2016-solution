35362bc22823fa4cb54a427e043b49e6782e0f6f
Anh <quanganh.tran94@gmail.com>
2016-06-23 00:23:55 +0300

    2-03

---
+++ Module_2-03_mastermind/src/source.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "source.h"
+
+
+#ifndef MAWKKE
+// longer solution by Pasi
+
+/* 03-mastermind
+ */
+void mastermind(const int *solution, const int *guess, char *result, unsigned int len)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        result[i] = ' ';
+    }
+    int count[10] = { 0 };
+    for (i = 0; i < len; i++) {
+        count[solution[i]]++;
+    }
+    for (i = 0; i < len; i++) {
+        if (guess[i] == solution[i]) {
+            result[i] = '+';
+            count[guess[i]]--;
+        }
+    }
+    for (i = 0; i < len; i++) {
+        if (result[i] == '+') continue;
+        if (count[guess[i]] > 0) {
+            result[i] = '*';
+            count[guess[i]]--;
+        } else {
+            result[i] = '-';
+        }
+    }
+}
+#endif

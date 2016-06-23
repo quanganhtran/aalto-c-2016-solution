3075ba03bc55372a41159e4a7a4a6ae869599a12
Anh <quanganh.tran94@gmail.com>
2016-06-23 12:57:47 +0300

    2-04

---
+++ Module_2-04_sort/src/source.c
@@ -0,0 +1,24 @@
+#include "source.h"
+
+
+/* Selection sort */
+/* Parameters:
+ * start: start of an array
+ * size: length of an array
+ */
+void sort(int *start, int size)
+{
+    for (int i = 0; i < size; i++) {
+        int el = start[i];
+        int pos;
+        for (pos = i - 1; pos >= 0; pos--) {
+            if (el < start[pos]) {
+                start[pos + 1] = start[pos];
+            } else {
+                break;
+            }
+        }
+        start[pos + 1] = el;
+    }
+}
+

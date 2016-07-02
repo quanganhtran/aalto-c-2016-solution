52a15dd89ae6599a49533adc5de5de5cdd6c5111
Anh <quanganh.tran94@gmail.com>
2016-07-02 18:15:35 +0300

    4-02

--- Module_4-02_life/src/gameoflife.c
+++ Module_4-02_life/src/gameoflife.c
@@ -1,6 +1,23 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "gameoflife.h"
 
+
+State** initCells(unsigned int xsize, unsigned int ysize) {
+    State** cells = calloc(ysize, sizeof(*(cells)));
+    for (unsigned int i = 0; i < ysize; i++) {
+        cells[i] = calloc(xsize, sizeof(**(cells)));
+    }
+    return cells;
+}
+
+void releaseCells(State** cells, unsigned int ysize) {
+    for (unsigned int y = 0; y < ysize; y++) {
+        free(cells[y]);
+    }
+    free(cells);
+}
 
 /* Exercise a: Allocates needed memory for the field structure and
  * the actual game field. 'xsize' and 'ysize' indicate the horizontal and
@@ -10,16 +27,19 @@
  */
 Field *createField(unsigned int xsize, unsigned int ysize)
 {
-    (void) xsize; // replace these lines
-    (void) ysize;
-    return NULL;   // replace this
+    Field* field = malloc(sizeof(Field));
+    field->xsize = xsize;
+    field->ysize = ysize;
+    field->cells = initCells(xsize, ysize);
+    return field;
 }
 
 /* Free memory allocated for field <f>.
  */
 void releaseField(Field *f)
 {
-    (void) f;
+    releaseCells(f->cells, f->ysize);
+    free(f);
 }
 
 /* Exercise b: Initialize game field by setting exactly <n> cells into
@@ -27,15 +47,28 @@ void releaseField(Field *f)
  */
 void initField(Field *f, unsigned int n)
 {
-    (void) f;
-    (void) n;
+    unsigned int total = f->xsize * f->ysize;
+    for (unsigned int i = 0; i < total; i++) {
+        if (rand() % (total - i) < n) {
+            f->cells[i / f->xsize][i % f->xsize] = ALIVE;
+            n--;
+            if (n <= 0)
+                return;
+        }
+    }
+    exit(EXIT_FAILURE);
 }
 
 /* Exercise c: Output the current state of field <f>.
  */
 void printField(const Field *f)
 {
-    (void) f;
+    for (unsigned int y = 0; y < f->ysize; y++) {
+        for (unsigned int x = 0; x < f->xsize; x++) {
+            printf("%c", f->cells[y][x] == ALIVE ? '*' : '.');
+        }
+        printf("\n");
+    }
 }
 
 
@@ -43,5 +76,44 @@ void printField(const Field *f)
  */
 void tick(Field *f)
 {
-    (void) f;
+    State** cells = f->cells;
+    State** stateAfter = initCells(f->xsize, f->ysize);
+    for (unsigned int y = 0; y < f->ysize; y++) {
+        for (unsigned int x = 0; x < f->xsize; x++) {
+            // Count neighbors
+            int neighbors = 0;
+            if (y > 0) {
+                neighbors += cells[y - 1][x] == ALIVE;
+                if (x > 0) {
+                    neighbors += cells[y - 1][x - 1] == ALIVE;
+                }
+                if (x < f->xsize - 1) {
+                    neighbors += cells[y - 1][x + 1] == ALIVE;
+                }
+            }
+            if (y < f->ysize - 1) {
+                neighbors += cells[y + 1][x] == ALIVE;
+                if (x > 0) {
+                    neighbors += cells[y + 1][x - 1] == ALIVE;
+                }
+                if (x < f->xsize - 1) {
+                    neighbors += cells[y + 1][x + 1] == ALIVE;
+                }
+            }
+            if (x > 0) {
+                neighbors += cells[y][x - 1] == ALIVE;
+            }
+            if (x < f->xsize - 1) {
+                neighbors += cells[y][x + 1] == ALIVE;
+            }
+            // Apply rules
+            if (cells[y][x] == ALIVE) {
+                stateAfter[y][x] = (2 <= neighbors && neighbors <= 3) ? ALIVE : DEAD;
+            } else {
+                stateAfter[y][x] = (neighbors == 3) ? ALIVE : DEAD;
+            }
+        }
+    }
+    releaseCells(f->cells, f->ysize);
+    f->cells = stateAfter;
 }

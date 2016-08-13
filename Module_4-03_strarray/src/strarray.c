c4471afe5fb8f5e0f1bfd234f2c44b6247f332ca
Anh <quanganh.tran94@gmail.com>
2016-08-13 22:07:46 +0300

    4-03

--- Module_4-03_strarray/src/strarray.c
+++ Module_4-03_strarray/src/strarray.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "strarray.h"
 
 /* Exercise a: Initializes the string array to contain the initial
@@ -21,7 +22,7 @@ void free_strings(char **array)
 {
     char** ite = array;
     while(*ite) {
-        free(ite);
+        free(*ite);
         ite++;
     }
     free(array);
@@ -32,14 +33,31 @@ void free_strings(char **array)
  */
 char **add_string(char **array, const char *string)
 {
-    size_t new_len = sizeof(array) / sizeof(*array) + 1;
-    array = realloc(array, sizeof(*array) * new_len);
-    
+    /* Get the index of the last element (the NULL pointer), which can determine
+     * the array current length. This operation is in O(n)
+     * Since no other information about the array is passed to the function,
+     * the only possible O(1) approach is to allocate extra memory in the array
+     * to store its current length, and offset accordingly. This might introduce
+     * bugs to the program.
+     */
+    int old_last = 0;
+    while(array[old_last]) {
+        old_last++;
+    }
+    // Increase the size of the array.
+    // Abort the program if this operation fails.
+    char** new_array = realloc(array, sizeof(*array) * (old_last + 2));
+    if (!new_array) {
+        free_strings(array);
+        exit(EXIT_FAILURE);
+    }
+    array = new_array;
+    // Copy the string to the new location, which is the previous last index.
     size_t str_len = strlen(string);
-    array[new_len - 2] = malloc(str_len + 1);
-    memcpy(array[new_len - 2], string, str_len + 1);
-    
-    array[new_len - 1] = NULL;
+    array[old_last] = malloc(str_len + 1);
+    memcpy(array[old_last], string, str_len + 1);
+    // Terminate the new array with a NULL pointer
+    array[old_last + 1] = NULL;
     return array;
 }
 
@@ -47,14 +65,31 @@ char **add_string(char **array, const char *string)
  */
 void make_lower(char **array)
 {
-    (void) array; // replace this
+    for (; *array; array++) {
+        char* str = *array;
+        for (; *str; str++) {
+            *str = tolower(*str);
+        }
+    }
 }
 
 
 /* Exercise d: reorder strings in <array> to lexicographical order */
 void sort_strings(char **array)
 {
-    (void) array;
+    // Insertion sort
+    for (int i = 0; array[i]; i++) {
+        char* el = array[i];
+        int pos;
+        for (pos = i - 1; pos >= 0; pos--) {
+            if (strcmp(el, array[pos]) < 0) {
+                array[pos + 1] = array[pos];
+            } else {
+                break;
+            }
+        }
+        array[pos + 1] = el;
+    }
 }
 
 /* You can use this function to check what your array looks like.
